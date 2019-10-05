23cb63e9e8c8ec2d1de33f61a8933044bed44ecc
nkmishra217 <deeksha.mishra_cs18@gla.ac.in>
2019-10-05 16:53:59 +0530

    add a loop to print every character using charAt

--- src/main/String1.java
+++ src/main/String1.java
@@ -8,14 +8,14 @@ public class String1 {
         String a = scanner.nextLine();
         int b = a.length();
         System.out.print("index:");
-        for (int i = 0; i <= b; i++) {
+        for (int i = 0; i < b; i++) {
             System.out.print(" ");
             System.out.print(i);
 
         }
         System.out.println("");
         System.out.print("char:");
-        for (int j = 0; j <= b; j++) {
+        for (int j = 0; j < b; j++) {
             System.out.print(" ");
             System.out.print(a.charAt(j));
         }
